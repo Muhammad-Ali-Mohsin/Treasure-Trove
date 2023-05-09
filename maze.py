92fc010491032982b717fbb73cf0f66539506bcb
Muhammad-Ali <66363462+Muhammad-Ali-Mohsin@users.noreply.github.com>
2023-05-09 18:45:08 +0100

    Added a border to the maze

--- maze.py
+++ maze.py
@@ -6,7 +6,7 @@ class Maze:
     def __init__(self, x, y, tile_size):
         self.resolution = (x, y)
         self._maze = [[1 for i in range(x)] for i in range(y)]
-        self.surface = pygame.Surface((x * tile_size, y * tile_size))
+        self.surface = pygame.Surface(((x + 2) * tile_size, (y + 2) * tile_size))
         self.tile_size = tile_size
 
     def draw(self, surface, camera_displacement):
@@ -22,8 +22,6 @@ class Maze:
 
         # Draws the maze surface onto the screen based on the camera displacement
         surface.blit(self.surface, (-camera_displacement[0], -camera_displacement[1]))
-
-        
 
     def change_cell(self, x, y, data):
         """
@@ -71,12 +69,11 @@ class Maze:
         return "\n".join([str(row) for row in self._maze])
     
 
-
 def generate_maze(x, y, tile_size):
     """
     Uses a modified version of Prim's algorithm to generate a maze
     """
-    maze = Maze(x=x, y=y, tile_size=tile_size)
+    maze = Maze(x=x-2, y=y-2, tile_size=tile_size)
     # Random starting cell
     starting_cell = (random.randint(0, maze.resolution[0] - 1), random.randint(0, maze.resolution[1] - 1))
     # List of cells to be explored and list of the cells that have been explored
@@ -106,5 +103,14 @@ def generate_maze(x, y, tile_size):
         # Remove the cell from the list of cells to be explored as it has been explored
         cells_list.remove(current_cell)
         explored_cells.add(current_cell)
+
+    for i in range(maze.resolution[1]):
+        maze._maze[i].insert(0, 1)
+        maze._maze[i].append(1)
+    
+    maze._maze.insert(0, [1 for i in range(x)])
+    maze._maze.append([1 for i in range(x)])
+    
+    maze.resolution = (x, y)
 
     return maze
