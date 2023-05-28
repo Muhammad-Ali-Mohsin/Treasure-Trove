a63d0241a95d695e35d4fd29f3adaddfe47c24b2
Muhammad-Ali <66363462+Muhammad-Ali-Mohsin@users.noreply.github.com>
2023-05-28 11:04:20 +0100

    Removed options and added them to main file

--- variables.py
+++ variables.py
@@ -1,14 +1,11 @@
 import pygame
 
 clock = pygame.time.Clock()
-monitor = pygame.display.Info()
-
-DEBUG_MODE = False
-FPS = 60
 
 # Constants
 MAZE_RESOLUTION = (50, 50)
 TILE_SIZE = 32
+PRIMARY_COLOUR = (255, 202, 24)
 
 # PLAYER CONSTANTS
 PLAYER_SIZE = 16
@@ -21,7 +18,6 @@ TILE_CENTER_SIZE = 18
 ENEMY_REFRESH_INTERVAL = 1
 
 # Display Constants
-USER_RESOLUTION = (monitor.current_w, monitor.current_h)
 SUPPORTED_RESOLUTIONS = ((3840, 2160), (2560, 1440), (1920, 1080), (1280, 720), (854, 480), (640, 360), (426, 240))
 GAME_RESOLUTION = (1920, 1080)
 MAZE_SURFACE_RESOLUTION = (645, 360)
@@ -29,7 +25,6 @@ COMPASS_SCALE = 5.5
 CURSOR_SCALE = 3
 
 # Variables about the display
-WINDOW = pygame.display.set_mode(USER_RESOLUTION)
 SCREEN = pygame.Surface(GAME_RESOLUTION)
 MAZE_SURFACE = pygame.Surface(MAZE_SURFACE_RESOLUTION)
 
@@ -39,9 +34,13 @@ COMPASS_SPINNER_IMG = pygame.image.load("assets/images/compass_spinner_img.png")
 ICON_IMG = pygame.image.load("assets/images/icon.png").convert_alpha()
 CURSOR_1 = pygame.image.load("./assets/images/cursor_1.png").convert_alpha()
 CURSOR_2 = pygame.image.load("./assets/images/cursor_0.png").convert_alpha()
+PAUSE_SCREEN_BOX_IMG = pygame.image.load("assets/images/box.png").convert_alpha()
 
 # Image Rescaling
 COMPASS_BASE_IMG = pygame.transform.scale(COMPASS_BASE_IMG, (COMPASS_SCALE * COMPASS_BASE_IMG.get_width(), COMPASS_SCALE * COMPASS_BASE_IMG.get_width()))
 COMPASS_SPINNER_IMG = pygame.transform.scale(COMPASS_SPINNER_IMG, (COMPASS_SCALE * COMPASS_SPINNER_IMG.get_width(), COMPASS_SCALE * COMPASS_SPINNER_IMG.get_width()))
 CURSOR_1 = pygame.transform.scale(CURSOR_1, (CURSOR_1.get_width() * CURSOR_SCALE, CURSOR_1.get_height() * CURSOR_SCALE))
 CURSOR_2 = pygame.transform.scale(CURSOR_2, (CURSOR_2.get_width() * CURSOR_SCALE, CURSOR_2.get_height() * CURSOR_SCALE))
+
+
+
