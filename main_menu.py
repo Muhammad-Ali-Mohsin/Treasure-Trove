0d315b686078fbd34fb5836abf50518ffba4cac1
Muhammad-Ali <66363462+Muhammad-Ali-Mohsin@users.noreply.github.com>
2023-05-28 11:03:18 +0100

    Added an options menu for changing resolution and fps

--- main_menu.py
+++ main_menu.py
@@ -4,24 +4,33 @@ from variables import *
 
 
 class MainMenu:
-    def __init__(self):
+    def __init__(self, window, fps):
         """
-        Creates all the starting game variables
+        Creates all the starting menu variables
         """
-        # Game Variables
-        self.fps = FPS
+        # Display Variables
+        self.screentype = "main menu"
+        self.selected_screen = "main menu"
+        self.window = window
+        self.user_resolution = (window.get_width(), window.get_height())
+        self.fps = fps
+        
+        # Player Variables
         self.clicked = False
-        self.mouse_pos = get_mouse_pos(USER_RESOLUTION, GAME_RESOLUTION)
+        self.mouse_pos = get_mouse_pos(self.user_resolution, GAME_RESOLUTION)
 
         # List of buttons [button label, selected screen]
-        self.buttons = [["Start Game", "game"], ["Options", "main menu"], ["Leaderboard", "main menu"], ["Credits", "main menu"], ["Exit to Desktop", "close"]]
+        self.buttons = [["Start Game", "game"], ["Options", "options menu"], ["Leaderboard", "main menu"], ["Credits", "main menu"], ["Exit to Desktop", "close"]]
         self.selected = None
-        self.selected_screen = "main menu"
 
         # Creates a rect for each button and adds it to the button's 2d list
         for i in range(len(self.buttons)):
-            rect = pygame.Rect((USER_RESOLUTION[0] // 2) - 300, ((i + 1) * 130) + 300, 600, 90)
+            self.buttons[i][0] = get_text_surf(size=60, text=self.buttons[i][0], colour=pygame.Color("white"))
+            rect = pygame.Rect(0, 0, 600, 65)
+            rect.center = ((GAME_RESOLUTION[0] // 2), 520 + (i + 1) * 85)
             self.buttons[i].append(rect)
+
+        self.title_text = get_text_surf(size=200, text="Treasure Trove", colour=PRIMARY_COLOUR)
 
 
 
@@ -32,16 +41,18 @@ class MainMenu:
         # Clears the screen
         SCREEN.fill((0, 0, 0))
 
+        # Blits the title
+        SCREEN.blit(self.title_text, ((GAME_RESOLUTION[0] // 2) - (self.title_text.get_width() // 2), 100))
+
         # Draws the buttons onto the screen
         for i, button in enumerate(self.buttons):
             if self.selected == i:
-                pygame.draw.rect(SCREEN, (255, 0, 0), button[2])
+                pygame.draw.rect(SCREEN, PRIMARY_COLOUR, button[2])
             else:
-                pygame.draw.rect(SCREEN, (255, 187, 0), button[2])
+                pygame.draw.rect(SCREEN, (255, 0, 0), button[2])
 
             # Draws the text onto the button
-            button_label = get_text_surf(size=80, text=button[0], colour=pygame.Color("white"))
-            SCREEN.blit(button_label, (button[2].centerx - (button_label.get_width() // 2), button[2].centery - (button_label.get_height() // 2)))
+            SCREEN.blit(button[0], (button[2].centerx - (button[0].get_width() // 2), button[2].centery - (button[0].get_height() // 2)))
 
         # Draws the cursor on to the screen
         SCREEN.blit(CURSOR_1, self.mouse_pos) if self.clicked else SCREEN.blit(CURSOR_2, self.mouse_pos)
@@ -51,13 +62,11 @@ class MainMenu:
         SCREEN.blit(fps_text, (10, 10))
 
         # Ouputs the display in the user resolution
-        WINDOW.blit(pygame.transform.scale(SCREEN, USER_RESOLUTION), (0, 0))
+        self.window.blit(pygame.transform.scale(SCREEN, self.user_resolution), (0, 0))
         pygame.display.update()
 
 
     def handle_events(self):
-        global USER_RESOLUTION
-        global WINDOW
         """
         Handles all input events such as key presses
         """
@@ -78,16 +87,16 @@ class MainMenu:
                         self.selected_screen = button[1]
 
             if event.type == pygame.MOUSEMOTION:
-                self.mouse_pos = get_mouse_pos(USER_RESOLUTION, GAME_RESOLUTION)
+                self.mouse_pos = get_mouse_pos(self.user_resolution, GAME_RESOLUTION)
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
 
-    def loop(self):
+    def run_frame(self):
         """
-        The game loop
+        Runs a frame of the main menu
         """
 
         # Checks whether the mouse is hovering over a button and selects it if so
---
+++ menus.py
@@ -0,0 +1,151 @@
+import pygame, sys
+from misc import get_text_surf, get_mouse_pos, update_options
+from variables import *
+
+class OptionsMenu:
+    def __init__(self, window, fps):
+        """
+        Creates all the starting menu variables
+        """
+        self.screentype = "options menu"
+        self.selected_screen = "options menu"
+        self.window = window
+        self.user_resolution = (window.get_width(), window.get_height())
+        # Game Variables
+        self.fps = fps
+        self.clicked = False
+        self.mouse_pos = get_mouse_pos(self.user_resolution, GAME_RESOLUTION)
+
+        # List of buttons [title, text on button, button rect]
+        self.buttons = [
+            ["Change Resolution", "<", (1220, 400, 100, 100), "decrease resolution"],
+            ["", ">", (1620, 400, 100, 100), "increase resolution"], 
+            ["Change FPS (Recommended 60)", "<", (1220, 600, 100, 100), "decrease fps"],
+            ["", ">", (1620, 600, 100, 100), "increase fps"], 
+            ["", "Return to Main Menu", ((GAME_RESOLUTION[0] // 2) - 300, 880, 600, 100), "main menu"]
+            ]
+        self.selected = None
+
+        # Creates a rect for each button and adds it to the button's 2d list
+        for i in range(len(self.buttons)):
+            self.buttons[i][0] = get_text_surf(size=60, text=self.buttons[i][0], colour=pygame.Color("white"))
+
+            self.buttons[i][1] = get_text_surf(size=60, text=self.buttons[i][1], colour=pygame.Color("white"))
+
+            rect = pygame.Rect(self.buttons[i][2])
+            self.buttons[i][2] = rect
+
+        self.title_text = get_text_surf(size=100, text="Option Menu", colour=PRIMARY_COLOUR)
+
+    def update_display(self):
+        """
+        Updates the screen
+        """
+        # Clears the screen
+        SCREEN.fill((0, 0, 0))
+
+        SCREEN.blit(self.title_text, ((GAME_RESOLUTION[0] // 2) - (self.title_text.get_width() // 2), 100))
+
+        # Draws the buttons onto the screen
+        for i, button in enumerate(self.buttons):
+            if self.selected == i:
+                pygame.draw.rect(SCREEN, PRIMARY_COLOUR, button[2])
+            else:
+                pygame.draw.rect(SCREEN, (255, 0, 0), button[2])
+
+            # Draws the title for the button
+            SCREEN.blit(button[0], (100, button[2].centery - (button[0].get_height() // 2)))
+            # Draws the text onto the button
+            SCREEN.blit(button[1], (button[2].centerx - (button[1].get_width() // 2), button[2].centery - (button[1].get_height() // 2)))
+
+        # Shows the current resolution between the resolution buttons
+        resolution_text = get_text_surf(size=55, text=f"{self.user_resolution[0]}x{self.user_resolution[1]}", colour=pygame.Color("white"))
+        SCREEN.blit(resolution_text, (1470 - (resolution_text.get_width() // 2), 450 - (resolution_text.get_height() // 2)))
+
+        # Shows the current fps between the fps buttons
+        fps_text = get_text_surf(size=55, text=f"{self.fps} FPS", colour=pygame.Color("white"))
+        SCREEN.blit(fps_text, (1470 - (fps_text.get_width() // 2), 650 - (fps_text.get_height() // 2)))
+
+        # Draws the cursor on to the screen
+        SCREEN.blit(CURSOR_1, self.mouse_pos) if self.clicked else SCREEN.blit(CURSOR_2, self.mouse_pos)
+
+        #Shows the FPS
+        fps_text = get_text_surf(size=55, text=f"FPS: {round(clock.get_fps())}", colour=pygame.Color("white"))
+        SCREEN.blit(fps_text, (10, 10))
+
+        # Ouputs the display in the user resolution
+        self.window.blit(pygame.transform.scale(SCREEN, self.user_resolution), (0, 0))
+        pygame.display.update()
+
+
+    def handle_events(self):
+        """
+        Handles all input events such as key presses
+        """
+        for event in pygame.event.get():
+            # Checks whether the X button has been pressed
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: self.clicked = True
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1: self.clicked = False
+                # Checks whether the mouse has clicked a button and if so, changes the selected screen
+                self.handle_buttons()
+
+            if event.type == pygame.MOUSEMOTION:
+                self.mouse_pos = get_mouse_pos(self.user_resolution, GAME_RESOLUTION)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        
+    def handle_buttons(self):
+        """
+        Checks which button has been pressed and runs the corresponding button
+        """
+        for button in self.buttons:
+            if button[2].collidepoint(self.mouse_pos):
+                if "resolution" in button[3]:
+                    # Gets the next supported resolution
+                    new_resolution = SUPPORTED_RESOLUTIONS[(SUPPORTED_RESOLUTIONS.index(self.user_resolution) + (-1 if button[3] == "increase resolution" else 1)) % len(SUPPORTED_RESOLUTIONS)]
+                    # Quits the display and then recreates it with the new resolution
+                    # This is necessary as pygame doesn't place the window in the centre unless I quit the display first
+                    self.user_resolution = new_resolution
+                    pygame.display.quit()
+                    pygame.display.init()
+                    self.window = pygame.display.set_mode(self.user_resolution)
+                    pygame.mouse.set_visible(False)
+                    pygame.display.set_caption("Treasure Trove")
+                    pygame.display.set_icon(ICON_IMG)
+
+                if "fps" in button[3]:
+                    # Changes the resolution
+                    self.fps = self.fps + (1 if button[3] == "increase fps" else -1)
+                
+                if button[3] == "main menu":
+                    # Updates the options to save the new options
+                    update_options(user_resolution=self.user_resolution, fps=self.fps)
+                    # Changes the screen back to the main menu
+                    self.selected_screen = "main menu"
+
+    def run_frame(self):
+        """
+        Runs a frame of the menu
+        """
+
+        # Checks whether the mouse is hovering over a button and selects it if so
+        self.selected = None
+        for i, button in enumerate(self.buttons):
+            if button[2].collidepoint(self.mouse_pos):
+                self.selected = i
+
+        # Calls functions
+        self.handle_events()
+        self.update_display()
+        clock.tick(self.fps)
+
